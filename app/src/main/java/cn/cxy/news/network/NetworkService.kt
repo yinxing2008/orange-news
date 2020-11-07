58986591148b870172148dc184dcba3002426613
jerry <309032663@qq.com>
2020-11-07 18:04:49 +0800

    update

--- app/src/main/java/cn/cxy/news/network/NetworkService.kt
+++ app/src/main/java/cn/cxy/news/network/NetworkService.kt
@@ -1,12 +1,15 @@
 package cn.cxy.news.network
 
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface NetworkService {
     @GET("wxarticle/chapters/json")
     suspend fun getChapters(): ChapterResp
 
-    @GET("wxarticle/list/408/1/json")
-    suspend fun getArticle(): ArticleResp
-
+    @GET("wxarticle/list/{chapterId}/{pageNum}/json")
+    suspend fun getArticle(
+        @Path("chapterId") chapterId: Int,
+        @Path("pageNum") pageNum: Int
+    ): ArticleResp
 }
